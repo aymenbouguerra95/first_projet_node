e657e0fcc54e31a3de4772b9cef0b21af74dfd9d
aymenbouguerra95 <aymen.bouguerra.95@gmail.com>
2025-01-16 19:31:07 +0100

    first projet node

---
+++ server.js
@@ -0,0 +1,27 @@
+const express=require('express');
+
+const app = express();
+
+app.post('/add',()=>{
+    console.log('add work');
+
+
+} );
+
+app.get('/getall', ()=>{
+    console.log('get work')
+} );
+
+app.put('/update',()=>{
+    console.log('update')
+});
+
+app.delete('/delete',()=>{
+    console.log('delete')
+
+
+});
+
+app.listen(3000 ,()=>{
+    console.log('hello aymen')
+});
