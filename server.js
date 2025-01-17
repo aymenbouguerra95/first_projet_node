6f5a8e2c3fea08705e44ca10085778f8eb55d212
aymenbouguerra95 <aymen.bouguerra.95@gmail.com>
2025-01-17 21:02:55 +0100

    add des user and find user in db

--- server.js
+++ server.js
@@ -1,17 +1,66 @@
 const express=require('express');
+const user = require("./models/user");
 require('./config/connect');   //appel les fichier connect for connect with mongodb
 const app = express();
-
-app.post('/add',()=>{
-    console.log('add work');
+app.use(express.json());
+//add in db
+app.post('/add',(req,res)=>{
+    data=req.body;
+    usr = new user(data);
+    usr.save()
+        .then(
+            (saveuser)=>{
+                res.send(saveuser)
+            }
+        )
+        .catch(
+            (err)=>{
+                res.send(err)
+            }
+        )
+    //console.log(data);
+    //console.log('add work');
 
 
 } );
 
-app.get('/getall', ()=>{
-    console.log('get work')
-} );
+app.post('/create',async(req,res)=>{
+    try{
 
+    data=req.body;
+    usr = new user(data);
+    saveduser = await usr.save();
+    res.send(saveduser)
+    }catch (error){
+        res.send(error)
+
+    }
+})
+
+//this for get donner for db 
+app.get('/getall', (req,res)=>{
+    user.find({ age:28 })
+        .then(
+            (user)=>{
+                res.send(user);
+            }
+
+        ).catch(
+            (err)=>{
+                res.send(err)
+            }
+        )
+    //console.log('get work')
+} );
+app.get('/all',async(req,res)=>{
+    try{
+       const usr=await user.find({age:29});
+        res.send(usr);
+
+    }catch(error){
+        res.send(error);
+    }
+})
 app.put('/update',()=>{
     console.log('update')
 });
